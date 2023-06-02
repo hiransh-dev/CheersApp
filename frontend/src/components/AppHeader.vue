59a29ed4ad414a134cbd65df5d768ff393c40984
Hiran Shah <hiran.shah4@gmail.com>
2023-06-02 14:53:16 +0100

    Frontpage banner

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,41 +1,10 @@
-<script>
-export default {
-  name: "AppHeader",
-  data() {
-    return {
-      searchBar: false /* 0 is Hidden, 1 is Show */,
-      screenSmall: true,
-      fullscreenMenu: false,
-      dialog: false,
-      all_locations: ["Central London", "Ilford", "Barking", "Stratford"]
-    };
-  },
-  methods: {
-    checkScreen() {
-      if (window.innerWidth <= 800) {
-        this.screenSmall = true;
-      } else {
-        this.screenSmall = false;
-        this.fullscreenMenu = false;
-      }
-    },
-    burgerClicked() {
-      this.fullscreenMenu = !this.fullscreenMenu;
-    }
-  },
-  mounted() {
-    /* Could use CSS media Queries with "display: contents/none" for this, but this help me check if the fullscreenMenu is open/close */
-    window.addEventListener("load", this.checkScreen);
-    window.addEventListener("resize", this.checkScreen);
-  }
-};
-</script>
-
 <template>
   <v-card>
     <v-layout>
       <v-app-bar class="navbar container bg-black" elevation="4">
-        <img class="logo_img" src="../assets/CheersLogo_nobg.png" />
+        <v-slide-x-transition>
+          <img class="logo_img" v-if="scrollValue" src="../assets/CheersLogo_nobg.png" />
+        </v-slide-x-transition>
         <v-app-bar-title></v-app-bar-title>
         <v-card
           v-if="!screenSmall"
@@ -43,24 +12,6 @@ export default {
           class="bg-black d-flex justify-end align-center"
           elevation="0"
         >
-          <v-responsive max-width="300">
-            <v-scroll-x-transition>
-              <v-card-text v-show="searchBar == 1">
-                <v-text-field
-                  class="w-5"
-                  density="compact"
-                  variant="outlined"
-                  label="Search"
-                  single-line
-                  hide-details
-                ></v-text-field>
-              </v-card-text>
-            </v-scroll-x-transition>
-          </v-responsive>
-          <v-btn @click.prevent="searchBar = !searchBar" icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-
           <v-btn rounded="lg" class="site_font text-none font-weight-bold mr-1 bg-yellow-darken-1"
             >Login</v-btn
           >
@@ -73,7 +24,7 @@ export default {
             <v-icon>mdi-cart-outline</v-icon>
           </v-btn>
         </v-card>
-        <v-card class="bg-black d-flex justify-end" v-else elevation="0">
+        <v-card class="bg-transparent d-flex justify-end" v-else elevation="0">
           <v-btn v-show="!fullscreenMenu" @click.prevent="burgerClicked" icon>
             <v-icon>mdi-menu</v-icon>
           </v-btn>
@@ -90,20 +41,6 @@ export default {
         temporary
       >
         <v-list class="d-flex flex-column align-end mr-5 mt-5">
-          <v-card class="w-100 d-flex flex-row justify-end" elevation="0">
-            <v-list-item class="w-75">
-              <v-text-field
-                density="compact"
-                variant="outlined"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
-            </v-list-item>
-            <v-btn elevation="0" icon>
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-          </v-card>
           <v-list-item>
             <v-btn elevation="0" icon>
               <v-icon>mdi-cart-outline</v-icon>
@@ -111,6 +48,7 @@ export default {
           </v-list-item>
           <v-list-item>
             <v-btn
+              size="large"
               rounded="lg"
               class="site_font text-none font-weight-bold bg-yellow-darken-1"
               elevation="0"
@@ -119,6 +57,7 @@ export default {
           </v-list-item>
           <v-list-item>
             <v-btn
+              size="large"
               rounded="lg"
               class="site_font text-none font-weight-bold bg-light-blue-lighten-1"
               elevation="0"
@@ -127,9 +66,27 @@ export default {
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
-      <v-main class="mt-5">
+
+      <v-main>
+        <v-parallax class="parallaxHeight" src="/cheers_banner.png" scale="0.5">
+          <div class="w-100 h-100 d-flex justify-center align-center">
+            <div :class="cheersBannerImgSize">
+              <v-img src="/CheersLogo_nobg.png"></v-img>
+            </div>
+          </div>
+        </v-parallax>
+        <!-- add one more card for pub location -->
+        <!-- CARD TO OPEN DIALOG -->
+        <div class="desktop">
+          <v-card class="mt-2 ma-2 bg-grey-darken-3 text-center desktop_container" elevation="24">
+            <v-card-title class="site_font text-white"
+              >Choose your pub location to order</v-card-title
+            >
+            <v-btn class="bg-black ma-2" size="large" @click="dialog = true">Choose Pub</v-btn>
+          </v-card>
+        </div>
         <div class="main container">
-          <v-btn @click="dialog = true">Select Location</v-btn>
+          <!-- LOCATION DIALOG WINDOW -->
           <v-dialog
             v-model="dialog"
             fullscreen
@@ -150,35 +107,43 @@ export default {
               <label class="site_font text-h4 text-center my-5 mx-5"
                 >Where do you want to place your order?</label
               >
-              <v-list>
-                <v-list-item>
-                  <v-btn
-                    class="bg-green-darken-2 w-100 site_font btn_font"
-                    size="large"
-                    elevation="24"
-                    prepend-icon="mdi-navigation-variant"
-                  >
-                    Enable Location
-                  </v-btn>
-                </v-list-item>
-              </v-list>
-              <v-divider></v-divider>
-              <v-list>
-                <v-list-item>
-                  <v-autocomplete
-                    class="mt-5"
-                    prepend-inner-icon="mdi-map-marker"
-                    label="Location Name"
-                    variant="outlined"
-                    :items="all_locations"
-                  ></v-autocomplete>
-                </v-list-item>
-                <v-list-item>
-                  <v-btn class="bg-black w-100 site_font btn_font" size="large" elevation="24">
-                    View all pubs
-                  </v-btn>
-                </v-list-item>
-              </v-list>
+
+              <div class="desktop">
+                <div class="desktop_container_small">
+                  <v-list>
+                    <v-list-item>
+                      <v-btn
+                        class="bg-green-darken-2 w-100 site_font btn_font"
+                        size="large"
+                        elevation="24"
+                        prepend-icon="mdi-navigation-variant"
+                      >
+                        Enable Location
+                      </v-btn>
+                    </v-list-item>
+                  </v-list>
+                </div>
+                <v-divider v-if="!screenSmall" vertical></v-divider>
+                <v-divider v-else></v-divider>
+                <div class="desktop_container_small">
+                  <v-list>
+                    <v-list-item>
+                      <v-autocomplete
+                        class="mt-5"
+                        prepend-inner-icon="mdi-map-marker"
+                        label="Location Name"
+                        variant="outlined"
+                        :items="all_locations"
+                      ></v-autocomplete>
+                    </v-list-item>
+                    <v-list-item class="desktop_container_small">
+                      <v-btn class="bg-black w-100 site_font btn_font" size="large" elevation="24">
+                        View all pubs
+                      </v-btn>
+                    </v-list-item>
+                  </v-list>
+                </div>
+              </div>
             </v-card>
           </v-dialog>
           <slot name="home"></slot>
@@ -188,6 +153,52 @@ export default {
   </v-card>
   <!-- bg-grey-lighten-1 -->
 </template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      screenSmall: true,
+      fullscreenMenu: false,
+      dialog: false,
+      scrollValue: false,
+      parallaxImgRatio: "1",
+      cheersBannerImgSize: "w-50",
+      all_locations: ["Central London", "Ilford", "Barking", "Stratford"]
+    };
+  },
+  methods: {
+    checkScreen() {
+      if (window.innerWidth <= 1024) {
+        this.screenSmall = true;
+        this.cheersBannerImgSize = "w-75";
+      } else {
+        this.screenSmall = false;
+        this.fullscreenMenu = false;
+        this.cheersBannerImgSize = "w-50";
+      }
+    },
+    burgerClicked() {
+      this.fullscreenMenu = !this.fullscreenMenu;
+    },
+    scrolled() {
+      if (window.scrollY <= 200) {
+        this.scrollValue = false;
+      } else {
+        this.scrollValue = true;
+      }
+    }
+  },
+  computed: {},
+  mounted() {
+    /* Could use CSS media Queries with "display: contents/none" for this, but this help me check if the fullscreenMenu is open/close */
+    window.addEventListener("load", this.checkScreen);
+    window.addEventListener("resize", this.checkScreen);
+    window.addEventListener("scroll", this.scrolled);
+  }
+};
+</script>
 
 <style scoped>
 .fullscreen {
@@ -205,5 +216,13 @@ export default {
 .site_font {
   font-family: "Roboto", sans-serif;
   /* font-family: "Dancing Script", cursive; */
+}
+.parallaxHeight {
+  height: 40vh;
+}
+@media only screen and (min-width: 1024px) {
+  .parallaxHeight {
+    height: 80vh;
+  }
 }
 </style>
--- frontend/src/components/AppHome.vue
+++ frontend/src/components/AppHome.vue
@@ -1,8 +1,19 @@
 <template>
   <AppHeader>
     <template v-slot:home>
-      <div class="home d-flex flex-column">
-        <h1>Welcome to Cheers!</h1>
+      <div class="home desktop">
+        <div class="desktop_container">
+          <v-card class="mt-2 w-100 d-flex flex-row justify-end" elevation="0">
+            <v-text-field
+              density="compact"
+              variant="outlined"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+            <v-btn icon="mdi-magnify" elevation="0"></v-btn>
+          </v-card>
+        </div>
       </div>
     </template>
   </AppHeader>
@@ -24,5 +35,13 @@ export default {
 
 <style scoped>
 .home {
+  display: flex;
+  flex-direction: column;
+}
+@media only screen and (min-width: 1024px) {
+  .home {
+    display: flex;
+    flex-direction: row;
+  }
 }
 </style>
