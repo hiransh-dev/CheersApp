81540da1630c9a9ec11500d6aaf00a882ace6319
Hiran Shah <hiran.shah4@gmail.com>
2023-06-17 02:47:47 +0100

    Changed router link from /drinks to /menu/drinks and so on

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,20 +2,28 @@
   <RouterLink :to="linkTo" class="router_link_decoration">
     <div class="home">
       <v-card class="home_card ma-1">
-        <v-img @click="linkTo = '/drinks'" src="../src/assets/images/drinks.png"></v-img>
-        <v-btn @click="linkTo = '/drinks'" class="bg-black" rounded="sm" size="x-large" block
+        <v-img @click="linkTo = '/menu/drinks'" src="../src/assets/images/drinks.png"></v-img>
+        <v-btn @click="linkTo = '/menu/drinks'" class="bg-black" rounded="sm" size="x-large" block
           >Drinks</v-btn
         >
       </v-card>
       <v-card class="home_card ma-1">
-        <v-img src="../src/assets/images/food.png"></v-img>
-        <v-btn @click="linkTo = '/food'" class="bg-black" rounded="sm" size="x-large" block
+        <v-img @click="linkTo = '/menu/food'" src="../src/assets/images/food.png"></v-img>
+        <v-btn @click="linkTo = '/menu/food'" class="bg-black" rounded="sm" size="x-large" block
           >Food & Snacks</v-btn
         >
       </v-card>
       <v-card class="home_card ma-1">
-        <v-img src="../src/assets/images/soft_drinks.png"></v-img>
-        <v-btn @click="linkTo = '/softdrinks'" class="bg-black" rounded="sm" size="x-large" block
+        <v-img
+          @click="linkTo = '/menu/softdrinks'"
+          src="../src/assets/images/soft_drinks.png"
+        ></v-img>
+        <v-btn
+          @click="linkTo = '/menu/softdrinks'"
+          class="bg-black"
+          rounded="sm"
+          size="x-large"
+          block
           >Soft Drinks</v-btn
         >
       </v-card>
