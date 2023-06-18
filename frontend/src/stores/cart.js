ecf23aca15492f65098b4e27a6ecef11810f329c
Hiran Shah <hiran.shah4@gmail.com>
2023-06-18 08:45:49 +0100

    Changed Store to save Cart total and calculate price in/for frontend

--- frontend/src/stores/cart.js
+++ frontend/src/stores/cart.js
@@ -3,17 +3,29 @@ import { defineStore } from "pinia";
 export default defineStore("cart", {
   state: () => {
     return {
-      cartItems: []
+      cartItems: [],
+      cartTotal: "0"
     };
   },
   actions: {
-    updateCart(id, quantity) {
+    updateCart(id, quantity, price) {
       if (this.cartItems.filter((item) => item.id === id)) {
         this.cartItems = this.cartItems.filter((item) => item.id !== id);
+        /* Verify price from backend after checkout is clicked */
+        this.updateCartTotal();
       }
       if (quantity !== 0) {
-        this.cartItems.push({ id, quantity });
+        this.cartItems.push({ id, quantity, price });
+        /* Verify price from backend after checkout is clicked */
+        this.updateCartTotal();
       }
+    },
+    updateCartTotal() {
+      /* Verify price from backend after checkout is clicked */
+      this.cartTotal = 0;
+      this.cartItems.forEach((item) => {
+        this.cartTotal = parseFloat(this.cartTotal + item.quantity * item.price);
+      });
     },
     clearCart() {
       this.cart = [];
