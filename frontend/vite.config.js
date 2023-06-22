4a4ec06a44511c92db21b80916bfe198c67fa3f7
Hiran Shah <hiran.shah4@gmail.com>
2023-06-22 11:25:49 +0100

    added changeOrigin to server options

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000"
+        target: "http://localhost:3000",
+        changeOrigin: true
       }
     }
   }
