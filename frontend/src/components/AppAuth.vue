2602e11cde914f7a8a5343cd5e018a24e342bcdf
Hiran Shah <hiran.shah4@gmail.com>
2023-06-23 00:14:03 +0100

    Connected frontend to backend User API to Login/Register.

--- frontend/src/components/AppAuth.vue
+++ frontend/src/components/AppAuth.vue
@@ -7,9 +7,9 @@
         <v-icon>mdi-close</v-icon>
       </v-btn>
     </v-toolbar>
-    <v-container class="text-center">
-      <v-card class="pa-2 bg-grey-lighten-2" elevation="8">
-        <v-row justify="center">
+    <v-container class="bg-grey-lighten-2">
+      <v-card class="pa-2 bg-grey-lighten-2 text-center" elevation="8">
+        <v-row justify="center" v-if="!authStore.email && authStore.email === ''">
           <v-col cols="6">
             <v-btn
               block
@@ -39,20 +39,39 @@
             </v-btn>
           </v-col>
         </v-row>
+        <v-row justify="center" v-if="authStore.email && authStore.email !== ''">
+          <v-col cols="12">
+            <v-btn
+              block
+              rounded="lg"
+              @click="fnLogout()"
+              class="text-black bg-yellow-darken-3"
+              size="large"
+              >Logout
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card>
-      <div>
-        <v-expand-transition>
-          <v-alert v-if="res_show_alert" :type="res_alert_variant" :title="res_alert_variant">
+      <div class="mt-5">
+        <v-slide-y-transition>
+          <v-alert
+            class="site_font"
+            v-if="res_show_alert"
+            :type="res_alert_variant"
+            :title="res_alert_variant"
+            closable
+          >
             {{ res_alert_message }}
           </v-alert>
-        </v-expand-transition>
+        </v-slide-y-transition>
       </div>
     </v-container>
-    <v-slide-y-reverse-transition mode="out-in" hide-on-leave="true">
-      <v-card class="bg-grey-lighten-2" v-if="tab === 'register'">
-        <v-card-text>
-          <veeForm @submit="register" :validation-schema="schema">
-            <v-container>
+    <!-- REGISTER PANEL -->
+    <v-container class="bg-grey-lighten-2">
+      <v-slide-y-reverse-transition mode="out-in" hide-on-leave="true">
+        <v-card class="bg-grey-lighten-2" v-if="tab === 'register' && !authStore.email">
+          <v-card-text>
+            <veeForm @submit="fnRegister" :validation-schema="schema">
               <v-row>
                 <v-col cols="12" sm="4">
                   <veeField id="regEmail" name="Email" v-slot="{ field, errorMessage }">
@@ -151,16 +170,17 @@
                 :loading="res_on_submit"
                 >Submit
               </v-btn>
-            </v-container>
-          </veeForm>
-        </v-card-text>
-      </v-card>
-    </v-slide-y-reverse-transition>
-    <v-slide-y-reverse-transition mode="in-out" hide-on-leave="true">
-      <v-card class="bg-grey-lighten-2" v-if="tab === 'login'">
-        <v-card-text>
-          <veeForm @submit="fnLogin" :validation-schema="schema">
-            <v-container>
+            </veeForm>
+          </v-card-text>
+        </v-card>
+      </v-slide-y-reverse-transition>
+    </v-container>
+    <!-- LOGIN PANEL -->
+    <v-container class="bg-grey-lighten-2">
+      <v-slide-y-reverse-transition mode="in-out" hide-on-leave="true">
+        <v-card class="bg-grey-lighten-2" v-if="tab === 'login' && !authStore.email">
+          <v-card-text>
+            <veeForm @submit="fnLogin">
               <v-row>
                 <v-col cols="12" sm="4">
                   <veeField id="loginEmail" name="Email" v-slot="{ field, errorMessage }">
@@ -202,17 +222,38 @@
                 :loading="res_on_submit"
                 >Submit
               </v-btn>
-            </v-container>
-          </veeForm>
-        </v-card-text>
-      </v-card>
-    </v-slide-y-reverse-transition>
+            </veeForm>
+          </v-card-text>
+        </v-card>
+      </v-slide-y-reverse-transition>
+    </v-container>
+    <!-- USER LOGGED IN - PANEL -->
+    <v-container class="bg-grey-lighten-2">
+      <v-slide-y-reverse-transition mode="in-out" hide-on-leave="true">
+        <v-card class="bg-grey-lighten-2" v-if="authStore.email && authStore.email !== ''">
+          <v-card-text>
+            <v-list class="bg-grey-lighten-2">
+              <v-list-item>
+                <label class="text-h3">Hello, {{ authStore.fullName }}</label>
+              </v-list-item>
+              <v-list-item>
+                <label class="text-h5">Email: {{ authStore.email }}</label>
+              </v-list-item>
+              <v-list-item>
+                <label class="text-h5">Phone: {{ authStore.phone }}</label>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-slide-y-reverse-transition>
+    </v-container>
   </v-dialog>
 </template>
 
 <script>
 import { mapStores } from "pinia";
 import usepageStore from "@/stores/page";
+import useauthStore from "@/stores/auth";
 import axios from "axios";
 
 export default {
@@ -245,15 +286,15 @@ export default {
     async fnRegister() {
       this.res_on_submit = true;
       try {
-        const addMenuItem = await axios.post(
-          "/api/management/menu",
+        const newUserCreated = await axios.post(
+          "/api/user/register",
           {
-            menuItem: {
-              title: this.title,
-              desc: this.desc,
-              price: this.price,
-              category: this.category,
-              subcategory: this.subcategory
+            registerUser: {
+              email: this.regEmail,
+              password: this.regPassword,
+              firstName: this.regfName,
+              lastName: this.reglName,
+              phoneNumber: this.regPhone
             }
           },
           {
@@ -262,14 +303,14 @@ export default {
             }
           }
         );
-        if (addMenuItem.status === 200) {
-          this.res_alert_message = `New Menu Item Added. \nTitle: ${addMenuItem.data.title}, \nDescription: ${addMenuItem.data.desc}, \nPrice: ${addMenuItem.data.price}, \nCategory: ${addMenuItem.data.category}, \nSub-category: ${addMenuItem.data.subcategory} `;
+        if (newUserCreated.status === 200) {
+          this.res_alert_message = `Your account has been created. Email: ${newUserCreated.data.email}, Name: ${newUserCreated.data.firstName} ${newUserCreated.data.lastName}`;
           this.res_alert_variant = "success";
           this.res_show_alert = true;
           this.res_on_submit = false;
         }
       } catch (e) {
-        this.res_alert_message = "Something went wrong";
+        this.res_alert_message = "Something went wrong. Account Creation Failed.";
         this.res_alert_variant = "error";
         this.res_show_alert = true;
         this.res_on_submit = false;
@@ -279,16 +320,11 @@ export default {
     async fnLogin() {
       this.res_on_submit = true;
       try {
-        const addMenuItem = await axios.post(
-          "/api/management/menu",
+        const userLoggedIn = await axios.post(
+          "/api/user/login",
           {
-            menuItem: {
-              title: this.title,
-              desc: this.desc,
-              price: this.price,
-              category: this.category,
-              subcategory: this.subcategory
-            }
+            username: this.loginEmail,
+            password: this.loginPassword
           },
           {
             headers: {
@@ -296,23 +332,42 @@ export default {
             }
           }
         );
-        if (addMenuItem.status === 200) {
-          this.res_alert_message = `New Menu Item Added. \nTitle: ${addMenuItem.data.title}, \nDescription: ${addMenuItem.data.desc}, \nPrice: ${addMenuItem.data.price}, \nCategory: ${addMenuItem.data.category}, \nSub-category: ${addMenuItem.data.subcategory} `;
+        if (userLoggedIn.status === 200) {
+          this.authStore.email = userLoggedIn.data.email;
+          this.authStore.firstName = userLoggedIn.data.firstName;
+          this.authStore.lastName = userLoggedIn.data.lastName;
+          this.authStore.fullName = userLoggedIn.data.fullName;
+          this.authStore.phone = userLoggedIn.data.phoneNumber;
+          this.res_alert_message = `Welcome Back ${userLoggedIn.data.fullName}.`;
           this.res_alert_variant = "success";
           this.res_show_alert = true;
-          this.res_on_submit = false;
+          // this.res_on_submit = false;
+          this.tab = "loggedin";
         }
       } catch (e) {
-        this.res_alert_message = "Something went wrong";
+        this.res_alert_message = "Something went wrong. Please try again.";
         this.res_alert_variant = "error";
         this.res_show_alert = true;
         this.res_on_submit = false;
-        // console.log(e);
+        console.log(e);
+      }
+    },
+    async fnLogout() {
+      try {
+        const logoutStatus = await axios.get("/api/user/logout");
+        if (logoutStatus.status === 200) {
+          this.authStore.clearAuth();
+          this.res_on_submit = false;
+          this.tab = "login";
+        }
+      } catch (e) {
+        console.log(e);
       }
     }
   },
   computed: {
     ...mapStores(usepageStore),
+    ...mapStores(useauthStore),
     phoneLength() {
       return "Phone Number: " + this.regPhone.length + "/10";
     }
