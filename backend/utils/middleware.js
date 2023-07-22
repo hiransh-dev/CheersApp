83d3d1f8e1c4be18423d032f7c5b46eea22f1cc2
Hiran Shah <hiran.shah4@gmail.com>
2023-07-22 14:55:42 +0100

    Middleware to check Login status

---
+++ backend/utils/middleware.js
@@ -0,0 +1,33 @@
+const isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return res.send("User not logged in");
+  }
+  next();
+};
+
+const isNotLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.send("User is already logged in");
+  }
+  next();
+};
+
+const isAuthor = (req, res, next) => {
+  if (req.body.userId !== req.user._id) {
+    return res.send("User is not the author");
+  }
+  next();
+};
+
+const isAdmin = (req, res, next) => {
+  if (x) {
+  }
+  next();
+};
+
+module.exports = {
+  isLoggedIn,
+  isNotLoggedIn,
+  //   isAuthor,
+  //   isAdmin,
+};
