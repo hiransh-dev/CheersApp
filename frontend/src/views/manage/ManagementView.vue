38481bc04d509c4bed71fc52ade6d298ff5a24a8
Hiran Shah <hiran.shah4@gmail.com>
2023-08-01 21:13:03 +0100

    Created ManagementView page

--- frontend/src/views/manage/ManagementView.vue
+++ frontend/src/views/manage/ManagementView.vue
@@ -1,18 +1,61 @@
 <template>
   <v-layout class="rounded rounded-md w-100 mt-2">
     <v-app-bar class="bg-yellow-darken-3" title="Management"></v-app-bar>
-    <v-navigation-drawer>
+    <v-navigation-drawer class="bg-black">
       <v-list>
-        <v-list-item title="Menu"></v-list-item>
+        <v-list-item>
+          <v-btn @click="mainContentComponent = 'AppOrdersPending'" variant="outlined" block>
+            Pending Orders
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn @click="mainContentComponent = 'AppOrdersHistory'" variant="outlined" block>
+            Orders History
+          </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn @click="mainContentComponent = 'AppUsers'" variant="outlined" block> Users </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn @click="mainContentComponent = 'AppStaff'" variant="outlined" block> Staff </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn @click="mainContentComponent = 'AppMenuManage'" variant="outlined" block>
+            Menu
+          </v-btn>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px">
-      Main Content
+    <v-main class="d-flex align-start justify-start ma-5" style="min-height: 100vh">
+      <v-fade-transition>
+        <component :is="mainContentComponent"></component>
+      </v-fade-transition>
     </v-main>
   </v-layout>
 </template>
 
-<script></script>
+<script>
+import AppMenuManage from "@/components/manage/AppMenuManage.vue";
+import AppOrdersPending from "@/components/manage/AppOrdersPending.vue";
+import AppOrdersHistory from "@/components/manage/AppOrdersHistory.vue";
+import AppStaff from "@/components/manage/AppStaff.vue";
+import AppUsers from "@/components/manage/AppUsers.vue";
+
+export default {
+  name: "managementView",
+  data() {
+    return {
+      mainContentComponent: "AppOrdersPending"
+    };
+  },
+  components: {
+    AppMenuManage,
+    AppOrdersPending,
+    AppOrdersHistory,
+    AppStaff,
+    AppUsers
+  }
+};
+</script>
 
 <style scoped></style>
