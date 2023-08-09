e6848c7f53831aa29372e371b93a91ec43c544b9
Hiran Shah <hiran.shah4@gmail.com>
2023-08-09 20:39:04 +0100

    Added Date Picker for order history to fetch orders from API by date.

--- frontend/src/components/manage/AppOrdersHistory.vue
+++ frontend/src/components/manage/AppOrdersHistory.vue
@@ -1,49 +1,63 @@
 <template>
-  <div class="d-flex flex-row flex-wrap">
-    <v-card
-      class="d-flex flex-column ma-2 pa-2 bg-blue-darken-4"
-      v-for="order of orderHistory"
-      :key="order._id"
-    >
-      <v-card-title>
-        <label>Table no: {{ order.tableNo }}</label
-        ><br />
-        <label>Order Total: £{{ order.orderTotal }}</label>
-      </v-card-title>
-      <v-card-subtitle>{{ order._id }}</v-card-subtitle>
-      <v-card-text>
-        <v-divider thickness="4" color="black"></v-divider>
-        Customer Name: {{ order.author.firstName }} {{ order.author.lastName }} <br />
-        Phone Number: {{ order.author.phoneNumber }} <br />
-        <v-divider thickness="4" color="black"></v-divider>
-        Ordered on: {{ readDate(order.createdAt) }}
-        <v-list rounded="lg" class="my-2 bg-black">
-          <v-list-item v-for="orderItem of order.orderItems" :key="orderItem">
-            <label>
-              {{ orderItem.item.title }}
-            </label>
-            <label> x{{ orderItem.quantity }} </label>
-          </v-list-item>
-        </v-list>
-      </v-card-text>
-    </v-card>
+  <div class="d-flex flex-column">
+    <v-date-picker
+      class="ma-2 bg-black"
+      v-model="date"
+      label="Date"
+      @click="getOrderHistory()"
+      required
+    ></v-date-picker>
+    <div class="d-flex flex-row flex-wrap">
+      <v-card
+        class="d-flex flex-column ma-2 pa-2 bg-blue-darken-4"
+        v-for="order of orderHistory"
+        :key="order._id"
+      >
+        <v-card-title>
+          <label>Table no: {{ order.tableNo }}</label
+          ><br />
+          <label>Order Total: £{{ order.orderTotal }}</label>
+        </v-card-title>
+        <v-card-subtitle>{{ order._id }}</v-card-subtitle>
+        <v-card-text>
+          <v-divider thickness="4" color="black"></v-divider>
+          Customer Name: {{ order.author.firstName }} {{ order.author.lastName }} <br />
+          Phone Number: {{ order.author.phoneNumber }} <br />
+          <v-divider thickness="4" color="black"></v-divider>
+          Ordered on: {{ readDate(order.createdAt) }}
+          <v-list rounded="lg" class="my-2 bg-black">
+            <v-list-item v-for="orderItem of order.orderItems" :key="orderItem">
+              <label>
+                {{ orderItem.item.title }}
+              </label>
+              <label> x{{ orderItem.quantity }} </label>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
+import { VDatePicker } from "vuetify/labs/VDatePicker";
+
 import axios from "axios";
-import { mapStores } from "pinia";
-import usePageStore from "@/stores/page";
 
 export default {
   data() {
     return {
-      orderHistory: []
+      orderHistory: [],
+      date: new Date()
     };
+  },
+  components: {
+    VDatePicker
   },
   methods: {
     async getOrderHistory() {
-      const OrderHistory = await axios.get("/api/order/allorders");
+      const OrderHistory = await axios.get(`/api/order/allorders/${this.date}`);
+      this.orderHistory = [];
       this.orderHistory = OrderHistory.data;
     },
     readDate(date) {
@@ -54,9 +68,6 @@ export default {
   },
   mounted() {
     this.getOrderHistory();
-  },
-  computed: {
-    ...mapStores(usePageStore)
   }
 };
 </script>
