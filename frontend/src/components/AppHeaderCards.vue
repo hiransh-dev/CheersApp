6a1146de73c4ebdef13f09cc0b25fafc2e5c8d05
Hiran Shah <hiran.shah4@gmail.com>
2023-08-14 06:40:15 +0100

    Added Header card to show User logged in and Orders with pending orders.

--- frontend/src/components/AppHeaderCards.vue
+++ frontend/src/components/AppHeaderCards.vue
@@ -1,8 +1,52 @@
 <template>
-  <!-- SELECT TABLE CARD -->
+  <!-- SHOW ACCOUNT CARD -->
+  <v-scale-transition>
+    <v-card
+      v-if="authStore.email !== ''"
+      class="w-100 bg-yellow-darken-3 text-center my-2 pa-2"
+      rounded="lg"
+      elevation="12"
+    >
+      <v-icon color="black" icon="mdi-badge-account" size="x-large"></v-icon>
+      <v-card-title class="site_font text-black text-h5">
+        Hello, {{ authStore.firstName }}
+      </v-card-title>
+      <!-- v-if="ordersStore.totalPendingOrders !== 0 && ordersStore.totalPendingOrders !== ''" -->
+      <v-badge color="red-darken-3" class="w-100" :content="ordersStore.totalPendingOrders">
+        <v-btn
+          class="bg-black site_font"
+          size="x-large"
+          rounded="lg"
+          @click="pageStore.dialogOrders = true"
+          block
+        >
+          View All Orders
+        </v-btn>
+      </v-badge>
+    </v-card>
+    <v-card
+      v-else
+      class="w-100 bg-yellow-darken-3 text-center my-2 pa-2"
+      rounded="lg"
+      elevation="12"
+    >
+      <v-icon color="black" icon="mdi-badge-account" size="x-large"></v-icon>
+      <v-card-title class="site_font text-black text-h5"> Welcome </v-card-title>
+      <v-btn
+        class="bg-black site_font"
+        size="x-large"
+        rounded="lg"
+        @click="pageStore.dialogAuth = true"
+        block
+      >
+        Login/Register
+      </v-btn>
+    </v-card>
+  </v-scale-transition>
+  <!-- SELECT TABLE CARD  -->
   <v-card
     v-if="pageStore.setTable !== 0"
-    class="bg-green-darken-3 text-center my-2 pa-2"
+    class="bg-blue-darken-3 text-center my-2 pa-2"
     rounded="lg"
     elevation="12"
   >
@@ -18,7 +62,7 @@
       {{ pageStore.setTable }}
     </v-btn>
   </v-card>
-  <v-card v-else class="bg-blue-grey-darken-3 text-center my-2 pa-2" rounded="lg" elevation="12">
+  <v-card v-else class="bg-brown-darken-3 text-center my-2 pa-2" rounded="lg" elevation="12">
     <v-icon color="black" icon="mdi-table-chair" size="x-large"></v-icon>
     <v-card-title class="site_font text-black">Choose your Table</v-card-title>
     <v-btn
@@ -33,7 +77,6 @@
   </v-card>
   <!-- SELECT TABLE DIALOG -->
   <v-dialog
-    class="bg-grey-lighten-2"
     v-model="dialogSetTable"
     fullscreen
     :scrim="false"
@@ -68,31 +111,55 @@
       </div>
     </v-card>
   </v-dialog>
-  <!-- SEARCH  -->
+  <!-- SEARCH BAR -->
   <!-- <label class="site_font text-h4 text-center my-5">Menu</label> -->
-  <v-card id="searchbar" class="my-4 w-100 d-flex flex-row justify-end" rounded="lg" elevation="6">
+  <!-- <v-card id="searchbar" class="my-4 w-100 d-flex flex-row justify-end" rounded="lg" elevation="6">
     <v-text-field variant="outlined" label="Search Menu" single-line hide-details></v-text-field>
     <v-btn icon="mdi-magnify" elevation="0"></v-btn>
-  </v-card>
-  <!-- ADD ORDER PENDING CARD  -->
-  <!-- MAYBE A LOGGED IN CARD  -->
+  </v-card> -->
   <slot></slot>
 </template>
 
 <script>
 import { mapStores } from "pinia";
 import usePageStore from "@/stores/page";
+import useAuthStore from "@/stores/auth";
+import useOrdersStore from "@/stores/orders";
 
 export default {
   name: "AppHeader",
   data() {
     return {
       dialogSetTable: false,
-      tables: 32
+      dialogOrderProcess: false,
+      tables: 32,
+      timerFunc: null
     };
   },
   computed: {
-    ...mapStores(usePageStore)
+    ...mapStores(usePageStore),
+    ...mapStores(useAuthStore),
+    ...mapStores(useOrdersStore)
+  },
+  methods: {
+    readDate(date) {
+      const fullDate = new Date(date);
+      const readableDate = `${fullDate.toDateString()}, ${fullDate.getHours()}:${fullDate.getMinutes()}`;
+      return readableDate;
+    },
+    timerFetchOrders() {
+      this.timerFunc = setInterval(() => {
+        this.ordersStore.fetchOrders();
+        this.ordersStore.fetchPendingOrders();
+      }, 5000);
+    }
+  },
+  mounted() {
+    this.ordersStore.fetchPendingOrders();
+    this.timerFetchOrders();
+  },
+  beforeUnmount() {
+    clearInterval(this.timerFunc);
   }
 };
 </script>
